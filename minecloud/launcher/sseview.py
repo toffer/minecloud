b37c99cc089e0cb3a9f6ff3c127097c0c63276b8
Tom Offermann <tom@offermann.us>
2013-05-30 14:53:21 -0700

    Make SSEView an iterator.
    * Use __iter__() and next() so that I can raise
      StopIteration when needed.
    * Previously, when clients disconnected, the infinite loop
      in iterator() caused a memory leak, so it's better to use
      next() so I can return a message at a time.
    * Track running time, so I can raise StopIteration
      when timeout is reached.

--- minecloud/launcher/sseview.py
+++ minecloud/launcher/sseview.py
@@ -3,26 +3,60 @@ import time
 
 from django import db
 from django.core.cache import cache
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+try:
+    from django.http import StreamingHttpResponse as HttpResponse
+except ImportError:
+    from django.http import HttpResponse
+
 from django_sse.views import BaseSseView
+from sse import Sse
 
 
 class SseView(BaseSseView):
 
-    def iterator(self):
+    def __init__(self):
+        self.sleep_interval = 3
+        self.start_time = time.time()
+        self.timeout = 30
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        running_time = time.time() - self.start_time
+        if running_time >= self.timeout:
+            raise StopIteration
+
+        time.sleep(self.sleep_interval)
+
+        # Set default, in case cache is empty
+        default = json.dumps(['instance_state', 'terminated'])
+        item = cache.get('instance_state', default)
+        key, value = json.loads(item)
+        self.sse.add_message(key, value)
+
+        return "".join(self.sse)
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
         # Close Django DB connection that handled auth query,
         # so it doesn't stay open until the end of the request
         # while SSEs are sent.
         db.close_connection()
 
-        # Set default, in case cache is empty
-        default = json.dumps(['instance_state', 'terminated'])
+        self.sse = Sse()
 
-        while True:
-            item = cache.get('instance_state', default)
-            key, value = json.loads(item)
-            self.sse.add_message(key, value)
-            yield
-            time.sleep(3)
+        self.request = request
+        self.args = args
+        self.kwargs = kwargs
+
+        response = HttpResponse(self, content_type="text/event-stream")
+        response['Cache-Control'] = 'no-cache'
+        response['Software'] = 'django-sse'
+        return response
 
 
 def send_event(event_name, data, key='instance_state'):
