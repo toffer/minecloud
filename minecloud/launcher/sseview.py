6fe7ff1daf7c0530def77da07e974a9713c485c1
Tom Offermann <tom@offermann.us>
2013-05-31 15:04:07 -0700

    Create SelfUpdatingSse to pass to HttpResponse.
    * Big change from last commit...SSEView is no longer an iterator.
    * Rather than make SSEView an iterator, create SelfUpdatingSse
      (subclass of Sse) as an iterator and pass it to HttpResponse.
    * Add EventReader base class.
    * Add CacheReader class (derived from EventReader.)
    * EventReader object has a configurable timeout and sleep_interval.
    * SelfUpdatingSse uses an EventReader object to read new events
      continually, yielding them as it reads them.
    * SelfUpdatingSse has a close() method that is called at the end of
      the request and is used for closing DB or Redis connections.

--- minecloud/launcher/sseview.py
+++ minecloud/launcher/sseview.py
@@ -15,30 +15,109 @@ from django_sse.views import BaseSseView
 from sse import Sse
 
 
-class SseView(BaseSseView):
+class EventReader(object):
+    """
+    Iterator that yields events to be consumed by an SSE object.
 
-    def __init__(self):
-        self.sleep_interval = 3
+    EventReader is a base class. To do anything useful, you must
+    implement a read_events() method that will actually get events.
+    Some possibile sources for events: polling memcache for updates,
+    subscribing to a Redis Pub/Sub channel, listening for Postgres
+    notifications.
+
+    EventReader also provides options for:
+
+        * Timout: Rather than providing an infinite stream of events,
+          EventReader can raise StopIteration after a configurable
+          timeout period.
+
+        * Sleep interval: Setting sleep_interval allows for a pause
+          between event readings. It's not necessary if you are listening
+          for events, but it's useful if you are polling for updates.
+
+    """
+    def __init__(self, timeout=30, sleep_interval=3):
         self.start_time = time.time()
-        self.timeout = 30
+        self.timeout = timeout                  # In seconds, or None.
+        self.sleep_interval = sleep_interval    # In seconds, or None.
+
+    def read_events(self):
+        """Customize this method to read events."""
+        raise NotImplementedError
 
     def __iter__(self):
-        return self
+        for event, data in self.read_events():
+            yield event, data
 
-    def next(self):
-        running_time = time.time() - self.start_time
-        if running_time >= self.timeout:
-            raise StopIteration
+            if self.timeout:
+                running_time = time.time() - self.start_time
+                if running_time >= self.timeout:
+                    self.close()
+                    raise StopIteration
 
-        time.sleep(self.sleep_interval)
+            if self.sleep_interval:
+                time.sleep(self.sleep_interval)
 
-        # Set default, in case cache is empty
-        default = json.dumps(['instance_state', 'terminated'])
-        item = cache.get('instance_state', default)
-        key, value = json.loads(item)
-        self.sse.add_message(key, value)
+    def close(self):
+        """Clean up any connections here (DB, Redis, etc.)."""
+        pass
 
-        return "".join(self.sse)
+
+class CacheReader(EventReader):
+    """EventReader that fetches events by polling Django cache."""
+
+    def __init__(self, key, default_value='', *args, **kwargs):
+        self.key = key
+        self.default_value = default_value
+        super(CacheReader, self).__init__(*args, **kwargs)
+
+    def read_events(self):
+        while True:
+            default = json.dumps([self.key, self.default_value])
+            item = cache.get(self.key, default)
+            event, data = json.loads(item)
+            yield event, data
+
+
+class SelfUpdatingSse(Sse):
+    """
+    Iterable object to be passed to StreamingHttpResponse.
+
+    Though SelfUpdatingSse is derived from the Sse class, it is used
+    differently. In the base Sse, __iter__() yields everything stored
+    in the buffer, then stops. In SelfUpdatingSse, __iter__() will
+    continually refresh its buffer by reading new events with
+    self.event_reader and continually yield them.
+
+    Because is yields all events, the SelfUpdatingSse object can itself be
+    passed as the iterator to StreamingHttpResponse, rather than creating a
+    separate iterator to read the events from the Sse object.
+
+    Plus, we can add a close() method, which will allow us to clean up any
+    connections necessary for reading events.
+
+    """
+    def __init__(self, event_reader, *args, **kwargs):
+        self.event_reader = event_reader
+        super(SelfUpdatingSse, self).__init__(*args, **kwargs)
+
+    def __iter__(self):
+        for event, data in self.event_reader:
+            self.add_message(event, data)
+            for item in self._buffer:
+                yield item
+            self.flush()
+
+    def close(self):
+        """
+        This is called by the WSGI server at the end of the request and
+        allows the reader to close any DB or Redis connections, as necessary.
+
+        """
+        self.event_reader.close()
+
+
+class SseView(BaseSseView):
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
@@ -47,13 +126,14 @@ class SseView(BaseSseView):
         # while SSEs are sent.
         db.close_connection()
 
-        self.sse = Sse()
-
+        reader=CacheReader(timeout=45, key='instance_state',
+                           default_value='terminated')
+        self.sse = SelfUpdatingSse(event_reader=reader)
         self.request = request
         self.args = args
         self.kwargs = kwargs
 
-        response = HttpResponse(self, content_type="text/event-stream")
+        response = HttpResponse(self.sse, content_type="text/event-stream")
         response['Cache-Control'] = 'no-cache'
         response['Software'] = 'django-sse'
         return response
